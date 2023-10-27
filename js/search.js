413b9484333b3ad1784c485b09c70a9b8a4b0ca5
duyapat-christony <148646726+duyapat-christony@users.noreply.github.com>
2023-10-27 21:45:47 +0800

    Delete js/search.js

--- js/search.js
+++
@@ -1,7 +0,0 @@
-const searcher = document.querySelector('.search')
-const searcher_button = document.querySelector('.btn')
-const input = document.querySelector('.input')
-searcher_button.addEventListener('click', () => {
-searcher.classList.toggle('active')
-input.focus()
-})
